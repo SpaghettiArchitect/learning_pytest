8d4eb204494c62374b9599d56f2f652f9e695417
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-18 00:36:25 -0600

    Add population functionality and tests
    - The get_formatted_city function now accepts an optional population
    parameter.
    
    - Added a new test to check if the function get_formatted_city correctly
    formats a city name if we include a population argument in the function
    call.

--- cities/city_functions.py
+++ cities/city_functions.py
@@ -1,4 +1,6 @@
-def get_formatted_city(city: str, country: str) -> str:
+def get_formatted_city(city: str, country: str, population: int = 0) -> str:
     """Create a neatly formatted city name."""
-    city_name = f"{city}, {country}"
-    return city_name.title()
+    city_name = f"{city}, {country}".title()
+    if population:
+        city_name += f" - population {population}"
+    return city_name
--- cities/test_cities.py
+++ cities/test_cities.py
@@ -5,3 +5,9 @@ def test_city_country():
     """Do cities like 'Santiago, Chile' work?"""
     formatted_city = get_formatted_city("santiago", "chile")
     assert formatted_city == "Santiago, Chile"
+
+
+def test_city_country_population():
+    """Do cities like 'Santiago, Chile - population 5000000' work?"""
+    formatted_city = get_formatted_city("santiago", "chile", population=5000000)
+    assert formatted_city == "Santiago, Chile - population 5000000"
