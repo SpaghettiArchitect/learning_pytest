e97041418e114639635204d2f87269aef7705eb8
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-17 23:49:28 -0600

    Add middle name functionality
    - The function get_formatted_name can take a middle name as an argument.

--- name_funtion.py
+++ name_funtion.py
@@ -1,4 +1,7 @@
-def get_formatted_name(first: str, last: str) -> str:
+def get_formatted_name(first: str, last: str, middle: str = "") -> str:
     """Generate a neatly formatted full name."""
-    full_name = f"{first} {last}"
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
     return full_name.title()
