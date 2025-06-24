951ca8eda7ddc6e13c89b74f0426ce9bce0b8ec0
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-23 22:02:18 -0600

    Replace employee instances with a fixture
    - The employee instances that are needed to assert each test have been
    replace with a single fixture.

--- employees/test_employee.py
+++ employees/test_employee.py
@@ -1,15 +1,21 @@
+import pytest
 from employee import Employee
 
 
-def test_give_default_raise():
-    """Test if the default raise is added to the employee's salary."""
+@pytest.fixture
+def new_employee():
+    """An employee that will be available to other tests."""
     new_employee = Employee("John", "Doe", 30_000)
+    return new_employee
+
+
+def test_give_default_raise(new_employee):
+    """Test if the default raise is added to the employee's salary."""
     new_employee.give_raise()
     assert new_employee.annual_salary == 35_000
 
 
-def test_give_custom_raise():
+def test_give_custom_raise(new_employee):
     """Test if a custom raise is added to the employee's salary."""
-    new_employee = Employee("John", "Doe", 30_000)
     new_employee.give_raise(10_000)
     assert new_employee.annual_salary == 40_000
